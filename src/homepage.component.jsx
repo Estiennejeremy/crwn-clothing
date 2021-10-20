adc91159f5ede6b4a0836c597031b20f83567c06
dash <jeremy.estienne@epitech.eu>
2021-10-20 14:31:41 +0200

    init commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './homepage.styles.scss'
+
+export const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'> Hats</h1>
+                    <span className='subtitle'>Shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'> women</h1>
+                    <span className='subtitle'>Shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'> men</h1>
+                    <span className='subtitle'>Shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'> god</h1>
+                    <span className='subtitle'>Shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'> petit chat</h1>
+                    <span className='subtitle'>Shop now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
