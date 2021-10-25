8ccd5af8596e444a33dac64e40e3abfe00647a4a
dash <jeremy.estienne@epitech.eu>
2021-10-25 10:24:07 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Switch , Route} from 'react-router-dom';
 
 import {HomePage}  from './pages/homepage/homepage.component'
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGES</h1>
+  </div>
+);
+
 function App() {
   return (
-      <HomePage></HomePage>
+      <div>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/hats' component={HatsPage}/>
+        </Switch>
+      </div>
   );
 }
 
